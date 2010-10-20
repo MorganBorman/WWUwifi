05da9d6f764e8a7830696197097bfa790c80431c
MorganBorman <morgan.borman@gmail.com>
2010-10-19 19:18:42 -0700

    Added more in TODO and made the Simple system detection work

--- System.py
+++ System.py
@@ -4,9 +4,9 @@ import platform
 def SystemType():
 	#should return 'win32' on windows and 'linux2' on linux
 	platforms = {'win32': 'windows', 'linux2': 'linux'}
-	if sys.platform() in platforms:
-		return platforms[sys.platform()]
-	else
+	if sys.platform in platforms:
+		return platforms[sys.platform]
+	else:
 		return 'loop'
 	
 	#returns something like ('Ubuntu', '9.10', 'karmic') on linux
